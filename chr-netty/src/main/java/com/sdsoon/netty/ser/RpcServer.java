6ce133430e330dcf028d2b2e69662aef7b1553a8
cyl5yy412 <cyl5yy412@gmail.com>
2019-04-22 09:54:20 +0800

    init

--- chr-netty/src/main/java/com/sdsoon/netty/ser/RpcServer.java
+++ chr-netty/src/main/java/com/sdsoon/netty/ser/RpcServer.java
@@ -51,6 +51,7 @@ public class RpcServer {
                     pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
                     pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));
                     pipeline.addLast("encoder", new ObjectEncoder());
+                    //禁用缓存
                     pipeline.addLast("decoder", new ObjectDecoder(Integer.MAX_VALUE, ClassResolvers.cacheDisabled(null)));
 
 
